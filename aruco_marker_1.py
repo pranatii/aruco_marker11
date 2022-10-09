b886f8fe5097b2950dfa91d4a361ae2a2c47e28a
pranatii <72647084+pranatii@users.noreply.github.com>
2022-10-09 21:06:30 +0530

    Update aruco_marker_1.py

--- aruco_marker_1.py
+++ aruco_marker_1.py
@@ -31,13 +31,13 @@ def image_masking(srcimg,destimg,dest_points):
     cv2.fillConvexPoly(destimg,dest_points,(255,255,255))
     wrap_img=wrap_img+destimg
     # cv2.imshow('wrap',wrap_img)
-    return wrap_img
+#     return wrap_img
     # results=cv2.bitwise_and(wrap_img,wrap_img,destimg,mask=mask)
-    # return results
-
+    return results
+# 
 while True:
     ret,frame=cap.read()
-    # _,src_vid=src_img.read()
+    _,src_vid=src_img.read()
     frame=cv2.resize(frame,(700,600))
     bbox,ids=find_aruco(frame,6,100)
     if bbox:
